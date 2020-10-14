b15e0c7d8c01288ad95739a13d0d45834689a351
laur80 <laur80raul@gmail.com>
2020-10-14 23:27:22 +0300

    Updates

---
+++ precache-manifest.765c248ca974ace0f7d28ebfc68c4fcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7b99beec0636ffe61aa",
+    "url": "/pokemon-proj/static/css/main.a33e7f62.chunk.css"
+  },
+  {
+    "revision": "c7b99beec0636ffe61aa",
+    "url": "/pokemon-proj/static/js/main.c7b99bee.chunk.js"
+  },
+  {
+    "revision": "ac5061da54c65d4799c1",
+    "url": "/pokemon-proj/static/js/1.ac5061da.chunk.js"
+  },
+  {
+    "revision": "e32f91d786f31edfbf3d",
+    "url": "/pokemon-proj/static/js/runtime~main.e32f91d7.js"
+  },
+  {
+    "revision": "8a555c11734d18e2153ba59bc114b7f5",
+    "url": "/pokemon-proj/index.html"
+  }
+];
